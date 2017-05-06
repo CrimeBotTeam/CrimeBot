e65ece2d1d86594f674af49b0f3e2883f6d30fba
jpedumont <jpedumont@gmail.com>
2017-05-05 23:22:39 -0700

    updating testJSON to fix error that was parsing the data twice

--- bin/testJSON.js
+++ bin/testJSON.js
@@ -6,8 +6,11 @@ const express = require('express');
 const request = require('request');
 
 var cardsJSON = require('../data/action_cards_starter.json');
-var actionDeck = JSON.parse(cardsJSON);
+//var actionDeck = JSON.parse(cardsJSON);
+//looks like the rquire command is making a good guess at the type
+//so i dont need to parse the json again
 
-console.log(actionDeck);
+console.log(cardsJSON);
+//console.log(actionDeck);
 
 //check error in json file (From compiler)
